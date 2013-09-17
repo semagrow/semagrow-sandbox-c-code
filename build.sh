775eebf74d44b270649b8ef0ccbe8285825771e7
jakobitschj <j.jakobitsch@semantic-web.at>
2013-09-17 17:12:02 +0200

    build.sh for c-code (configure + make)

---
+++ build.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+echo "running configure"
+./configure
+if [ $? == 0 ]; then
+    echo "configure complete"
+    echo "running make"
+    make
+    makeExit=$?;
+    if [ $makeExit != 0 ]; then
+        echo "error running make"
+        exit 1;
+    else
+        echo "running make complete"
+        exit 0;
+    fi
+else
+    echo "error running configure"
+    exit 1;
+fi
